5a3f0fe013ba2c8550e9a55db282ae7b9f661aa2
syuchan1005 <syuchan.dev@gmail.com>
2017-04-17 23:20:10 +0900

    :bug: themeにあるファイルをフォルダとして認識してしまう

--- module/ThemeLoader.js
+++ module/ThemeLoader.js
@@ -17,19 +17,21 @@ class ThemeLoader {
         fs.readdir(themeFolder, async function (err, folders) {
             folders.forEach(function (folder_name) {
                 const folder = Path.join(themeFolder, folder_name);
-                const json = JSON.parse(fs.readFileSync(Path.join(folder, "theme.json")));
-                sass.render({
-                    file: Path.join(folder, "main.scss"),
-                    outputStyle: "compressed",
-                    sourceMap: true
-                }, function (err, result) {
-                    const saveCSS = Path.join(folder, `main.css`);
-                    if (!fs.existsSync(saveCSS)) {
-                        fs.writeFileSync(saveCSS, result.css);
-                        fs.writeFileSync(`${saveCSS}.map`, result.map);
-                    }
-                    connector.addTheme(folder_name, json.name, json.description, json.version);
-                });
+                if (fs.statSync(folder).isDirectory()) {
+                    const json = JSON.parse(fs.readFileSync(Path.join(folder, "theme.json")));
+                    sass.render({
+                        file: Path.join(folder, "main.scss"),
+                        outputStyle: "compressed",
+                        sourceMap: true
+                    }, function (err, result) {
+                        const saveCSS = Path.join(folder, `main.css`);
+                        if (!fs.existsSync(saveCSS)) {
+                            fs.writeFileSync(saveCSS, result.css);
+                            fs.writeFileSync(`${saveCSS}.map`, result.map);
+                        }
+                        connector.addTheme(folder_name, json.name, json.description, json.version);
+                    });
+                }
             });
         });
     }
